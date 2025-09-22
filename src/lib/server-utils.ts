00dd0a98b3da61960bdf7b5279414311d906980b
Daniel Wei <weidaniel15@gmail.com>
2025-09-22 16:00:51 -0400

    Format server-utils

--- src/lib/server-utils.ts
+++ src/lib/server-utils.ts
@@ -1,19 +1,18 @@
-
 import path from "path";
-import fs from 'fs/promises';
+import fs from "fs/promises";
 // import neatCsv from 'neat-csv';
 
 export async function getCsvData(csv: string) {
-    try {
-      const newPath = path.join(process.cwd(), 'src', 'assets', `${csv}.csv`);
-      let csvData = await fs.readFile(newPath, 'utf-8');
-      // Remove BOM if present
-      if (csvData.charCodeAt(0) === 0xFEFF) {
-        csvData = csvData.slice(1);
-      }
-      return csvData;
-    } catch (error) {
-      console.error('Error reading CSV file:', error);
-      return null;
+  try {
+    const newPath = path.join(process.cwd(), "src", "assets", `${csv}.csv`);
+    let csvData = await fs.readFile(newPath, "utf-8");
+    // Remove BOM if present
+    if (csvData.charCodeAt(0) === 0xfeff) {
+      csvData = csvData.slice(1);
     }
+    return csvData;
+  } catch (error) {
+    console.error("Error reading CSV file:", error);
+    return null;
   }
+}
