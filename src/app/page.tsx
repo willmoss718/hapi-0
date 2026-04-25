76b536f39ae69dd4b4ca8d00b4178cb167393c9f
willmoss718 <willmoss718@gmail.com>
2026-04-25 15:29:45 -0400

    home page edit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ export default async function Home() {
   return (
     <>
       <h1 className="text-4xl mt-8 md:text-5xl font-medium md:mt-16">
-        AI in Healthcare Policy: Laws, Regulations, and Standards (HAPI)
+        AI in Healthcare Policy: Laws, Regulations, and Standards
       </h1>
       <h2 className="text-lg mt-4 text-balance w-full">
         The Health & AI Policy Index (HAPI) tracks laws, regulations, and standards shaping artificial intelligence in healthcare across U.S. states, federal agencies, and global frameworks. Explore policies, trends, and real-world implications for health systems, developers, and policymakers.
