e93793836d5a5d76a24eabb3dc9622ee3e5c144f
willmoss718 <willmoss718@gmail.com>
2026-05-10 15:05:35 -0400

    Lock map and sidebar sizing during hover

--- src/components/map-sidebar-shell.tsx
+++ src/components/map-sidebar-shell.tsx
@@ -24,8 +24,8 @@ export default function MapSidebarShell({
   const activeState = hoveredState ? stateIntelligence[hoveredState] : null;
 
   return (
-    <div className="flex flex-col gap-6 pr-8 md:flex-row md:gap-8">
-      <div id="map" className="w-full h-full">
+    <div className="flex flex-col items-start gap-6 pr-8 md:flex-row md:gap-8">
+      <div id="map" className="w-full min-w-0 flex-1 h-full">
         <Map
           hoveredState={hoveredState}
           statePolicyCounts={statePolicyCounts}
@@ -33,7 +33,10 @@ export default function MapSidebarShell({
         />
       </div>
 
-      <div id="about" className="w-full md:w-96 h-full flex flex-col gap-4">
+      <div
+        id="about"
+        className="w-full flex-none md:w-96 md:min-w-[24rem] md:max-w-[24rem] h-full flex flex-col gap-4"
+      >
         {activeState ? <StateIntelligencePanel state={activeState} /> : <WhatsNew />}
       </div>
     </div>
